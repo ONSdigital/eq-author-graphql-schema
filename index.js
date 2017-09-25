ff87872089cbfde430e3dbe9e90c1301fa01ae5d
Nick Williams <WickyNilliams@users.noreply.github.com>
2017-09-25 17:22:50 +0300

    Improve graphql usage (#10)
    * cleanup: remove deprecations
    
    * add `newId` field to all types
    
    * allows us to transition to using ID type
    * old `id: Int` field remains until clients updated
    
    * create input types for all mutations
    
    * make id fields optional during transition
    
    * format with prettier

--- index.js
+++ index.js
@@ -1,95 +1,94 @@
 module.exports = `
-
 scalar Date
 
 type Questionnaire {
-    id: Int
-    title: String
-    description: String
-    theme: Theme
-    legalBasis: LegalBasis
-    navigation: Boolean
-    surveyId: String
-    createdAt: Date
-    sections: [Section]
-    groups: [Section] @deprecated(reason: "use 'sections' instead")
+  id: Int
+  newId: ID
+  title: String
+  description: String
+  theme: Theme
+  legalBasis: LegalBasis
+  navigation: Boolean
+  surveyId: String
+  createdAt: Date
+  sections: [Section]
 }
 
 type Section {
-    id: Int
-    title: String!
-    description: String
-    pages: [Page]
-    questionnaireId: Int! @deprecated(reason: "use 'questionnaire' instead")
-    questionnaire: Questionnaire
+  id: Int
+  newId: ID
+  title: String!
+  description: String
+  pages: [Page]
+  questionnaire: Questionnaire
 }
 
 interface Page {
-    id: Int!
-    title: String!
-    description: String
-    pageType: PageType!
-    sectionId: Int! @deprecated(reason: "use 'section' instead")
-    section: Section
+  id: Int
+  newId: ID
+  title: String!
+  description: String
+  pageType: PageType!
+  section: Section
 }
 
 type QuestionPage implements Page {
-    id: Int!
-    title: String!
-    description: String!
-    guidance: String
-    pageType: PageType!
-    answers:  [Answer]
-    sectionId: Int! @deprecated(reason: "use 'section' instead")
-    section: Section
+  id: Int
+  newId: ID
+  title: String!
+  description: String!
+  guidance: String
+  pageType: PageType!
+  answers: [Answer]
+  section: Section
 }
 
 interface Answer {
-    id: Int!
-    description: String
-    guidance: String
-    qCode: String
-    label: String
-    type: AnswerType!
-    mandatory: Boolean
-    questionPageId: Int! @deprecated(reason: "use 'page' instead")
-    page: QuestionPage
+  id: Int
+  newId: ID
+  description: String
+  guidance: String
+  qCode: String
+  label: String
+  type: AnswerType!
+  mandatory: Boolean
+  page: QuestionPage
 }
 
 type BasicAnswer implements Answer {
-    id: Int!
-    description: String
-    guidance: String
-    qCode: String
-    label: String
-    type: AnswerType!
-    mandatory: Boolean
-    questionPageId: Int! @deprecated(reason: "use 'page' instead")
-    page: QuestionPage
+  id: Int
+  newId: ID
+  description: String
+  guidance: String
+  qCode: String
+  label: String
+  type: AnswerType!
+  mandatory: Boolean
+  page: QuestionPage
 }
 
 type MultipleChoiceAnswer implements Answer {
-    id: Int!
-    description: String
-    guidance: String
-    qCode: String
-    label: String
-    type: AnswerType!
-    mandatory: Boolean
-    options: [Option]
-    questionPageId: Int! @deprecated(reason: "use 'page' instead")
-    page: QuestionPage
+  id: Int
+  newId: ID
+  description: String
+  guidance: String
+  qCode: String
+  label: String
+  type: AnswerType!
+  mandatory: Boolean
+  options: [Option]
+  page: QuestionPage
 }
 
 type Option {
-    id: Int!
-    label: String
-    description: String
-    value: String
-    qCode: String
-    childAnswerId: Int
-    answerId: Int! @deprecated(reason: "use 'answer' instead")
-    answer: Answer
+  id: Int
+  newId: ID
+  label: String
+  description: String
+  value: String
+  qCode: String
+  childAnswerId: Int
+  answer: Answer
 }
 
 enum PageType {
@@ -98,66 +97,260 @@ enum PageType {
 }
 
 enum AnswerType {
-    Checkbox
-    Currency
-    Date
-    MonthYearDate
-    Number
-    Percentage
-    Radio
-    TextArea
-    TextField
-    Relationship
+  Checkbox
+  Currency
+  Date
+  MonthYearDate
+  Number
+  Percentage
+  Radio
+  TextArea
+  TextField
+  Relationship
 }
 
 enum LegalBasis {
-    Voluntary
-    StatisticsOfTradeAct
+  Voluntary
+  StatisticsOfTradeAct
 }
 
 enum Theme {
-    default
-    census
+  default
+  census
 }
 
 type Query {
-    questionnaires: [Questionnaire]
-    questionnaire(id: Int!): Questionnaire
-    section(id: Int!): Section
-    group(id: Int!): Section @deprecated(reason: "use 'section' instead")
-    page(id: Int!): Page
-    questionPage(id: Int!): QuestionPage
-    answer(id: Int!): Answer
-    option(id: Int!): Option
+  questionnaires: [Questionnaire]
+  questionnaire(id: Int, newId: ID): Questionnaire
+  section(id: Int, newId: ID): Section
+  page(id: Int, newId: ID): Page
+  questionPage(id: Int, newId: ID): QuestionPage
+  answer(id: Int, newId: ID): Answer
+  option(id: Int, newId: ID): Option
 }
 
 type Mutation {
-    # creates a Questionnaire along with an initial Section and Page
-    createQuestionnaire(title: String!, description: String, theme: String!, legalBasis: LegalBasis!, navigation: Boolean, surveyId: String!) : Questionnaire
-    updateQuestionnaire(id: Int!, title: String, description: String, theme: String, legalBasis: LegalBasis, navigation: Boolean, surveyId: String) : Questionnaire
-    deleteQuestionnaire(id: Int!) : Questionnaire
-
-    createSection(title: String!, description: String, questionnaireId: Int!) : Section
-    updateSection(id: Int!, title: String, description: String) : Section
-    deleteSection(id: Int!) : Section
-    createGroup(title: String!, description: String, questionnaireId: Int!) : Section @deprecated(reason: "use 'createSection' instead")
-    updateGroup(id: Int!, title: String, description: String) : Section @deprecated(reason: "use 'updateSection' instead")
-    deleteGroup(id: Int!) : Section @deprecated(reason: "use 'deleteSection' instead")
-
-    createPage(title: String!, description: String, sectionId: Int!) : Page
-    updatePage(id: Int!, title: String!, description: String) : Page
-    deletePage(id: Int!) : Page
-
-    createQuestionPage(title: String!, description: String, guidance: String, sectionId: Int!) : QuestionPage
-    updateQuestionPage(id: Int!, title: String, description: String, guidance: String) : QuestionPage
-    deleteQuestionPage(id: Int!) : QuestionPage
-
-    createAnswer(description: String, guidance: String, label: String, qCode: String, type: AnswerType!, mandatory: Boolean!, questionPageId: Int!) : Answer
-    updateAnswer(id: Int!, description: String, guidance: String, label: String, qCode: String, type: AnswerType, mandatory: Boolean) : Answer
-    deleteAnswer(id: Int!) : Answer
-    
-    createOption(label: String, description: String, value: String, qCode: String, childAnswerId: Int, answerId: Int!) : Option
-    updateOption(id: Int!, label: String, description: String, value: String, qCode: String, childAnswerId: Int) : Option
-    deleteOption(id: Int!) : Option
+  # creates a Questionnaire along with an initial Section and Page
+  createQuestionnaire(
+    title: String
+    description: String
+    theme: String
+    legalBasis: LegalBasis
+    navigation: Boolean
+    surveyId: String
+    input: CreateQuestionnaireInput
+  ): Questionnaire
+  updateQuestionnaire(
+    id: Int
+    title: String
+    description: String
+    theme: String
+    legalBasis: LegalBasis
+    navigation: Boolean
+    surveyId: String
+    input: UpdateQuestionnaireInput
+  ): Questionnaire
+  deleteQuestionnaire(id: Int, input: DeleteQuestionnaireInput): Questionnaire
+  createSection(
+    title: String
+    description: String
+    questionnaireId: Int
+    input: CreateSectionInput
+  ): Section
+  updateSection(
+    id: Int
+    title: String
+    description: String
+    input: UpdateSectionInput
+  ): Section
+  deleteSection(id: Int, input: DeleteSectionInput): Section
+  createPage(
+    title: String
+    description: String
+    sectionId: Int
+    input: CreatePageInput
+  ): Page
+  updatePage(
+    id: Int
+    title: String
+    description: String
+    input: UpdatePageInput
+  ): Page
+  deletePage(id: Int, input: DeletePageInput): Page
+  createQuestionPage(
+    title: String
+    description: String
+    guidance: String
+    sectionId: Int
+    input: CreateQuestionPageInput
+  ): QuestionPage
+  updateQuestionPage(
+    id: Int
+    title: String
+    description: String
+    guidance: String
+    input: UpdateQuestionPageInput
+  ): QuestionPage
+  deleteQuestionPage(id: Int, input: DeleteQuestionPageInput): QuestionPage
+  createAnswer(
+    description: String
+    guidance: String
+    label: String
+    qCode: String
+    type: AnswerType
+    mandatory: Boolean
+    questionPageId: Int
+    input: CreateAnswerInput
+  ): Answer
+  updateAnswer(
+    id: Int
+    description: String
+    guidance: String
+    label: String
+    qCode: String
+    type: AnswerType
+    mandatory: Boolean
+    input: UpdateAnswerInput
+  ): Answer
+  deleteAnswer(id: Int, input: DeleteAnswerInput): Answer
+  createOption(
+    label: String
+    description: String
+    value: String
+    qCode: String
+    childAnswerId: Int
+    answerId: Int
+    input: CreateOptionInput
+  ): Option
+  updateOption(
+    id: Int
+    label: String
+    description: String
+    value: String
+    qCode: String
+    childAnswerId: Int
+    input: UpdateOptionInput
+  ): Option
+  deleteOption(id: Int, input: DeleteOptionInput): Option
 }
+
+input CreateQuestionnaireInput {
+  title: String!
+  description: String
+  theme: String!
+  legalBasis: LegalBasis!
+  navigation: Boolean
+  surveyId: String!
+}
+
+input UpdateQuestionnaireInput {
+  id: ID!
+  title: String
+  description: String
+  theme: String
+  legalBasis: LegalBasis
+  navigation: Boolean
+  surveyId: String
+}
+
+input DeleteQuestionnaireInput {
+  id: ID!
+}
+
+input CreateSectionInput {
+  title: String!
+  description: String
+  questionnaireId: ID!
+}
+
+input UpdateSectionInput {
+  id: ID!
+  title: String
+  description: String
+}
+
+input DeleteSectionInput {
+  id: ID!
+}
+
+input CreatePageInput {
+  title: String!
+  description: String
+  sectionId: ID!
+}
+
+input UpdatePageInput {
+  id: ID!
+  title: String!
+  description: String
+}
+
+input DeletePageInput {
+  id: ID!
+}
+
+input CreateQuestionPageInput {
+  title: String!
+  description: String
+  guidance: String
+  sectionId: ID!
+}
+
+input UpdateQuestionPageInput {
+  id: ID!
+  title: String
+  description: String
+  guidance: String
+}
+
+input DeleteQuestionPageInput {
+  id: ID!
+}
+
+input CreateAnswerInput {
+  description: String
+  guidance: String
+  label: String
+  qCode: String
+  type: AnswerType!
+  mandatory: Boolean!
+  questionPageId: ID!
+}
+
+input UpdateAnswerInput {
+  id: ID!
+  description: String
+  guidance: String
+  label: String
+  qCode: String
+  type: AnswerType
+  mandatory: Boolean
+}
+
+input DeleteAnswerInput {
+  id: ID!
+}
+
+input CreateOptionInput {
+  label: String
+  description: String
+  value: String
+  qCode: String
+  childAnswerId: ID
+  answerId: ID!
+}
+
+input UpdateOptionInput {
+  id: ID!
+  label: String
+  description: String
+  value: String
+  qCode: String
+  childAnswerId: ID
+}
+
+input DeleteOptionInput {
+  id: ID!
+}
+
 `;
