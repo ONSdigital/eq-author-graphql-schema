2fcca367a38dcecbf3f746a8fe76f4815b214700
Samiwel Thomas <samiwel.thomas@gmail.com>
2018-06-07 16:27:21 +0100

    Remove goto from create routing rule input.

--- index.js
+++ index.js
@@ -429,7 +429,6 @@ input ResetRoutingRuleSetElseInput {
 input CreateRoutingRuleInput {
   operation: RoutingOperation!
   routingRuleSetId: ID!
-  goto: RoutingDestinationInput
 }
 
 input UpdateRoutingRuleInput {
